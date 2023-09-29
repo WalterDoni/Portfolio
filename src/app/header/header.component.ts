48a40e16fedbdb29b3bbcd43df39bcebd9f11d00
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2023-09-29 20:17:47 +0200

    connect links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,6 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
+
+
 export class HeaderComponent {
+  burgermenu = false;
+
+
+  showMenu() {
+
+    if (this.burgermenu == false) {
+      this.burgermenu = true;
+      console.log(this.burgermenu);
+
+    } else {
+      this.burgermenu = false;
+      console.log(this.burgermenu);
+    }
+  }
+
+
 
 }
--- src/app/headline-section/headline-section.component.ts
+++ src/app/headline-section/headline-section.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class HeadlineSectionComponent {
 
+  
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView();
+}
 }
